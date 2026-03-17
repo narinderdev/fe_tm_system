5efdb5fc7a5231f0541a16a71c306f00eb70ad19
narinderdev <narinder.dev@gmail.com>
2026-03-17 14:59:48 +0530

    Fix live not working issue

--- src/app/interceptors/server-error-toast.interceptor.ts
+++ src/app/interceptors/server-error-toast.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import {
   HttpErrorResponse,
   HttpEvent,
@@ -12,14 +12,19 @@ import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ServerErrorToastInterceptor implements HttpInterceptor {
-  constructor(private readonly toastr: ToastrService) {}
+  constructor(
+    private readonly toastr: ToastrService,
+    private readonly ngZone: NgZone
+  ) {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(req).pipe(
       catchError((error) => {
         if (req.method && ['POST', 'PATCH', 'DELETE'].includes(req.method.toUpperCase()) && error instanceof HttpErrorResponse) {
           const serverMessage = this.extractMessage(error);
-          this.toastr.error(serverMessage, 'Error');
+          this.ngZone.run(() => {
+            this.toastr.error(serverMessage, 'Error');
+          });
         }
         return throwError(() => error);
       })
